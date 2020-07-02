0e1254daf73686542f5cdf5474659535558fdfec
AndVikVin <vintskevich@gmail.com>
2020-07-02 14:40:51 +0300

    fix:source code update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,19 @@
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat:{
+            options: {
+                separator: ';'
+            },
+            dist:{
+                src:['src/*.js'],
+                dest: '<%= pkg.name %>.js'
+            }
+        },
         uglify: {
             main: {
                 files: {
-                    '<%= pkg.name %>.min.js': './src/<%= pkg.name %>.js'
+                    '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
             }
         },
@@ -30,8 +39,9 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask("default", ["uglify", "cssmin"])
+    grunt.registerTask("default", ["concat", "uglify", "cssmin"])
 
 };
