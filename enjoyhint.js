dc428de9de73303ba38cf0fe461ef8e98c52099c
AZhurav1ev <azhuravlev@xbsoftware.by>
2020-07-21 12:32:46 +0300

    Added new enjoyHint vertical space calculating conditions for small devices

--- enjoyhint.js
+++ enjoyhint.js
@@ -1220,8 +1220,8 @@
           var left_offset = data.center_x - half_w;
           var right_offset = body_size.w - (data.center_x + half_w);
 
-          var label_shift = 150;
-          var label_margin = 40;
+          var label_shift = window.innerHeight < 670 ? 130 : 150;
+          var label_margin = window.innerHeight < 670 ? 0 : 40;
           var label_shift_with_label_height =
             label_shift + label_height + label_margin;
           var label_ver_offset = half_h + label_shift;
@@ -1237,7 +1237,7 @@
             {name: 'center_bottom', common_area: window.innerWidth * bottom_offset, width: window.innerWidth, height: bottom_offset},
           ];
           var label_horizontal_space_required = label_width;
-          var label_vertical_space_required = label_shift_with_label_height + 20;
+          var label_vertical_space_required = window.innerHeight <= 670 ? label_shift_with_label_height : label_shift_with_label_height + 20;
 
           var areas_priority = areas_for_label
             .sort(function(area1, area2){return area1.common_area - area2.common_area})
